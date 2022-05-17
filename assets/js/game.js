d74a739baef35936bd6034c9620c262713de27cc
ARJUN <arjunsharman06@gmail.com>
2022-05-17 17:51:42 -0400

    Logged the information of COnsole Log

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,14 @@
-window.alert("This is an alert! Javascript is running!")
+//this create a function named "fight"
+function fight(){
+    window.alert("The fight has began");
+}
+
+var playerName=window.prompt("What is the name of your robot?")
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+//fight();
